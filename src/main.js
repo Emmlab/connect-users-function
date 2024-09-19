331c539939049fc2c6e9f0a9542fc66270ab6ead
EmmanuelBeja <beja.emmanuel@gmail.com>
2024-09-19 17:24:44 +0300

    modified usersfunction

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import { users } from './functions/usersfunction';
+const sdk = require('node-appwrite');
+
+const client = new sdk.Client()
+  .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+  .setProject(req.env.APPWRITE_PROJECT_ID) // Your project ID
+  .setKey(req.env.APPWRITE_USERS_FUNCTION_SECRET_KEY); // Your secret API key
 
 export default async ({ req, res, log, error }) => {
   if (req.method === 'GET') {
     try {
+      const users = new sdk.Users(client);
       log('Ping');
       const result = await users.list();
       return res.send(result, 200);
